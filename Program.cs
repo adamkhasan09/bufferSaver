9b4a57cc75c95d8cf7efabd58e0002e44211bac8
adamkhasan09 <tigerko9999@gmail.com>
2020-08-10 12:46:56 +0300

    message

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace wordSaver
+{
+    class Program
+    {
+        [STAThread] //атрибут однопоточного прилжения Single Thread Apartment
+        static void Main(string[] args)
+        {
+            string txt = Clipboard.GetText();
+            StreamWriter f = new StreamWriter("Сборник.txt",true);
+            Console.WriteLine(txt);
+            if( txt.Trim(' ') != "")
+            {
+                f.WriteLine(txt);
+                f.Close();
+                succsess("11111111111111111111111111111");
+               
+            }
+            void succsess(string msg)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(msg);
+                }
+            }
+        }
+    }
+}
