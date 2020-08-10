eac792ffe0873e2942db8b40b6d2f78adc0756cd
adamkhasan09 <tigerko9999@gmail.com>
2020-08-10 12:09:28 +0300

    first commit

---
+++ wordSaver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace wordSaver
+{
+    class Program
+    {
+        [STAThread] //атрибут однопоточного прилжения Single Thread Apartment
+        static void Main(string[] args)
+        {
+            string txt = Clipboard.GetText();
+            StreamWriter f = new StreamWriter("Сборник.txt");
+            f.WriteLine(txt);
+            f.Close();
+        }
+    }
+}
